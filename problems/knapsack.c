ec4e4071ff66b7bc4c597236f804b446ff0bad20
mrbesher <besher333@outlook.sa>
2021-12-06 19:54:19 +0300

    Add item printing

--- problems/knapsack.c
+++ problems/knapsack.c
@@ -55,6 +55,17 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
+void print_taken_items(int** matrix, Item* itemsArr, size_t size, int maxWeight) {
+    size_t i, currWeight;
+    currWeight = maxWeight;
+    for (i = 0; i < size; i++) {
+        if (matrix[size - i][currWeight] != matrix[size - i - 1][currWeight]) {
+            printf("Get the item #%lu (v:%d, w:%d)\n", size - i, itemsArr[size - i - 1].value, itemsArr[size - i - 1].weight);
+            currWeight -= itemsArr[size - i - 1].weight;
+        }
+    }
+}
+
 int main() {
     size_t size, i, j;
     Item* itemsArr;
@@ -67,14 +78,17 @@ int main() {
     matrix = init_matrix(size, maxWeight);
     for (i = 1; i < size + 1; i++) {
         for (j = 1; j < (size_t)maxWeight + 1; j++) {
-            matrix[i][j] = (itemsArr[i - 1].weight > (int)j) ? matrix[i - 1][j]
-                                                             : max(itemsArr[i - 1].value + matrix[i - 1][j - itemsArr[i - 1].weight], matrix[i - 1][j]);
+            if (itemsArr[i - 1].weight > (int)j) {
+                matrix[i][j] = matrix[i - 1][j];
+            } else {
+                matrix[i][j] = max(itemsArr[i - 1].value + matrix[i - 1][j - itemsArr[i - 1].weight], matrix[i - 1][j]);
+            }
         }
     }
 
-    printf("Max value possible: %d\n", matrix[size][maxWeight]);
-    /* TODO: */
-    /* print_taken_items(matrix); */
+    printf("Max. value possible: %d. Hurray!\n", matrix[size][maxWeight]);
+    print_taken_items(matrix, itemsArr, size, maxWeight);
+    printf("This is the optimal solution, turst me.\nHurry up!\n");
 
     for (i = 0; i < size + 1; i++) {
         free(matrix[i]);
